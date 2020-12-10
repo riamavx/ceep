28f052e7cb553c9198ed4d75f3f448ad001d2fdd
Maria Souza <messenger.mariah2017@gmail.com>
2020-12-10 15:28:27 -0300

    btn de excluir funcionando

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import "./assets/App.css"
+import "./assets/App.css";
 import './assets/index.css';
-
 class App extends Component {
+
   constructor(){
     super();
-    
-    this.state={
+
+    this.state = {
       notas:[]
     }
   }
+
   criarNota(titulo, texto){
     const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoArrayNotas = [...this.state.notas,novaNota]
     const novoEstado = {
-      notas: novoArrayNotas
+      notas:novoArrayNotas
     }
     this.setState(novoEstado)
-
   }
-  render(){
+
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas:arrayNotas})
+  }
+
+  render() {
     return (
       <section className="conteudo">
-      <FormularioCadastro criarNota= {this.criarNota.bind(this)}/>
-      <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas 
+        apagarNota={this.deletarNota.bind(this)}
+        notas={this.state.notas}/>
       </section>
     );
-
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-import deleteSVG from "../../assets/img/delete.svg"
-import "./cardNota.css"
-
+import "./cardNota.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg"
 class CardNota extends Component {
+
+  apagar(){
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+  
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
-    <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-    <img src={deleteSVG} onClick={()=>console.log("Delete")} alt=""/>
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteSVG onClick={this.apagar.bind(this)}/>
         </header>
-    <p className="card-nota_texto">{this.props.texto}</p>
+        <p className="card-nota_texto">{this.props.texto}</p>
+
       </section>
     );
   }
